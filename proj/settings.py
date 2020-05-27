b0eb5eb6411fd7b87582f6af073fe42fded6adeb
SimKennedy <simkennedy1@gmail.com>
2020-05-27 19:06:30 +0200

    Add waitress and whitenoise for Heroku

--- proj/settings.py
+++ proj/settings.py
@@ -120,6 +120,7 @@ MIDDLEWARE = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'htmlmin.middleware.HtmlMinifyMiddleware',
     'htmlmin.middleware.MarkRequestMiddleware',
 )
