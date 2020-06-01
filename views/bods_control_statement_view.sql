05448236bf035b98cd22b3af99a7755c4b96acef
ErinClark <erinhmclark@hotmail.com>
2020-06-01 17:15:07 +0100

    bods views added

---
+++ views/bods_control_statement_view.sql
@@ -0,0 +1,23 @@
+
+-- CREATE OR REPLACE VIEW bods_control_statement_view AS
+SELECT
+        b.statement_json -> 'subject' ->> 'describedByEntityStatement' AS subject_entity_statement,
+        -- This will need expanding for real datasets
+        b.statement_json -> 'interests' -> 0 ->> 'type' AS interest_type,
+        b.statement_json -> 'interests' -> 0 -> 'share' -> 'exact' AS exact_share,
+        b.statement_json -> 'interests' -> 0 ->> 'startDate' AS interest_start_date,
+        b.statement_json -> 'interests' -> 0 ->> 'interestLevel' AS interest_level,
+        b.statement_json -> 'interests' -> 0 ->> 'beneficialOwnershipOrControl' AS beneficial_ownership_or_ontrol,
+        b.statement_json ->> 'isComponent' AS is_component,
+        b.statement_json ->> 'statementID' AS statement_id,
+        b.statement_json ->> 'statementType' AS statement_type,
+        NULLIF((b.statement_json ->> 'statementDate'::text), ''::text) AS statement_date,
+        b.statement_json -> 'interestedParty' ->> 'describedByPersonStatement' AS interested_person_statement,
+        b.statement_json -> 'interestedParty' ->> 'describedByEntityStatement' AS interested_person_statement,
+        b.statement_json -> 'publicationDetails' -> 'publisher' ->> 'name' AS publisher_name,
+        NULLIF(((b.statement_json -> 'publicationDetails'::text) ->> 'publicationDate'::text), ''::text) AS publication_date,
+        b.statement_json -> 'publicationDetails' ->> 'bodsVersion' AS bods_version
+       FROM
+         bods_json b
+WHERE b.statement_json -> 'statementType' = '"ownershipOrControlStatement"'
+;
---
+++ views/bods_person_view.sql
@@ -0,0 +1,20 @@
+
+CREATE OR REPLACE VIEW bods_tenders_view AS
+SELECT
+       -- Multiple names will need to be split
+        b.statement_json -> 'names' -> 0 ->> 'fullName' AS Name,
+        b.statement_json ->> 'personType' AS entity_type,
+       -- Multiple ids will need to be split
+        b.statement_json -> 'identifiers' -> 0 ->> 'id' AS entity_id,
+        b.statement_json -> 'identifiers' -> 0 ->> 'schemeName' AS country,
+        b.statement_json ->> 'isComponent' AS is_component,
+        b.statement_json ->> 'statementID' AS statement_id,
+        b.statement_json ->> 'statementType' AS statement_type,
+        NULLIF((b.statement_json ->> 'statementDate'::text), ''::text) AS statement_date,
+        b.statement_json -> 'publicationDetails' -> 'publisher' ->> 'name' AS publisher_name,
+        NULLIF(((b.statement_json -> 'publicationDetails'::text) ->> 'publicationDate'::text), ''::text) AS publication_date,
+        b.statement_json -> 'publicationDetails' ->> 'bodsVersion' AS bods_version
+       FROM
+         bods_json b
+WHERE b.statement_json -> 'statementType' = '"personStatement"'
+;
