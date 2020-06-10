36da1defbfd94fe6bd5ea315761f8af988607dca
SimKennedy <simkennedy1@gmail.com>
2020-06-10 18:40:19 +0200

    Remove test_view and move original prototype views to localhost:8000/prototype/

--- bluetail/views.py
+++ bluetail/views.py
@@ -11,17 +11,6 @@ from bluetail.models import BODSPersonStatementJSON, BODSEntityStatementJSON, BO
     BODSOwnershipStatement, BODSPersonStatement
 
 
-def test_view(request):
-    example_ocds_path = os.path.join(settings.BASE_DIR, "example_files", "ocds_tenderers.json")
-    example_ocds_json = json.load(open(example_ocds_path))
-    json_pretty = json.dumps(example_ocds_json, sort_keys=True, indent=4)
-    context = {
-        "ocds_full": example_ocds_json,
-        "ocds_full_pretty": json_pretty,
-    }
-    return render(request, "test.html", context)
-
-
 def tenders_view(request):
     context = {
         "tenders": [
