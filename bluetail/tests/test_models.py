2538f52bb3cd56a571f98489f83dbc60f45fab53
SimKennedy <simkennedy1@gmail.com>
2020-06-15 14:41:03 +0200

    Fix test_models.py::OcdsReleaseTestCase::test_release() for Github tests

--- bluetail/tests/test_models.py
+++ bluetail/tests/test_models.py
@@ -1,16 +1,15 @@
 import json
 import os
 
-# from django.conf import settings
+from django.conf import settings
 from django.test import TestCase
-from bluetail.models import OCDSReleaseJSON
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+from bluetail.models import OCDSReleaseJSON
 
 
 class OcdsReleaseTestCase(TestCase):
     def setUp(self):
-        example_ocds_path = os.path.join(BASE_DIR, "../../example_files", "ocds_tenderers.json")
+        example_ocds_path = os.path.join(settings.BASE_DIR, "example_files", "ocds_tenderers.json")
         example_ocds_json = json.load(open(example_ocds_path))
 
         OCDSReleaseJSON.objects.create(
@@ -20,5 +19,5 @@ class OcdsReleaseTestCase(TestCase):
         )
 
     def test_release(self):
-        release = OCDSReleaseJSON.objects.get(name="ocds-123abc-PROC-20-0001")
+        release = OCDSReleaseJSON.objects.get(ocid="ocds-123abc-PROC-20-0001")
         self.assertEqual(release.release_id, "PROC-20-0001-02-tender")
