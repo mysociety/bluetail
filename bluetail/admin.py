ae4f215fb3ca2c57bf14fb5d01e287bc721f2453
Chris Mytton <chrismytton@gmail.com>
2020-06-17 17:01:23 +0100

    Prevent flag_name from being changed in the admin
    This identifier will likely end up appearing in code. We don't want an
    accidental change in the admin to break things.
    
    This means that flag names will need to be changed using Django's shell
    or in the database directly.

--- bluetail/admin.py
+++ bluetail/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 
 from bluetail.models import Flag, OCDSReleaseJSON, FlagAttachment
 
-admin.site.register(Flag)
+
+class FlagAdmin(admin.ModelAdmin):
+    fields = ('flag_name', 'flag_field', 'flag_text', 'flag_type')
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ['flag_name']
+        else:
+            return []
+
+
+admin.site.register(Flag, FlagAdmin)
 admin.site.register(FlagAttachment)
 admin.site.register(OCDSReleaseJSON)
